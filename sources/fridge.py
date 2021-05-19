d607337d178949d43f176f111de0de81fdf39cdc
Pantomik <addr@domain.ext>
2021-05-19 10:30:10 +0200

    Fridge can count

--- sources/fridge.py
+++ sources/fridge.py
@@ -36,6 +36,7 @@ class Fridge:
         self._ingredients: Dict[str, int] = dict.fromkeys(ALL_INGREDIENTS, default_ingredients)
         self._sauces: Dict[str, int] = dict.fromkeys(ALL_SAUCES, default_sauce)
         self._count: Dict[str, int] = dict.fromkeys(ALL_INGREDIENTS, 0)
+        
 
     @property
     def is_empty(self) -> bool:
